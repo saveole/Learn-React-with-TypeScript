f5d04f10e5fdb84bae775253ea8b121818f0d6ef
saveole <xgz.cz@foxmail.com>
2023-08-11 14:40:03 +0800

    React Hooks - useReducer for complex state management

--- ch4/sec1-effect-hook/app/src/PersonScore.tsx
+++ ch4/sec1-effect-hook/app/src/PersonScore.tsx
@@ -1,16 +1,46 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer } from 'react';
 import { getPerson } from './getPerson';
 
+type State = {
+  name: string | undefined;
+  score: number;
+  loading: boolean;
+};
+
+// these action objects represent all the eays in which state can change
+type Action =
+  | { type: 'initialize'; name: string }
+  | { type: 'increment' }
+  | { type: 'decrement' }
+  | { type: 'reset' };
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case 'initialize':
+      return { name: action.name, score: 0, loading: false };
+    case 'decrement':
+      return { ...state, score: state.score - 1 };
+    case 'increment':
+      return { ...state, score: state.score + 1 };
+    case 'reset':
+      return { ...state, score: 0 };
+    default:
+      return state;
+  }
+}
+
 export function PersonScore() {
-  const [name, setName] = useState<string | undefined>();
-  const [score, setScore] = useState(0);
-  const [loading, setLoading] = useState(true);
+  const [{ name, score, loading }, dispatch] = useReducer(reducer, {
+    name: undefined,
+    score: 100,
+    loading: false,
+  });
+  // const [name, setName] = useState<string | undefined>();
+  // const [score, setScore] = useState(0);
+  // const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getPerson().then((person) => {
-      setLoading(false);
-      setName(person.name);
-    });
+    getPerson().then(({ name }) => dispatch({ type: 'initialize', name }));
   }, []);
 
   if (loading) {
@@ -21,9 +51,9 @@ export function PersonScore() {
       <h3>
         {name}, {score}
         <br />
-        <button onClick={() => setScore(score + 1)}>Add</button>
-        <button onClick={() => setScore(score - 1)}>Subtract</button>
-        <button onClick={() => setScore(0)}>Reset</button>
+        <button onClick={() => dispatch({ type: 'increment' })}>Add</button>
+        <button onClick={() => dispatch({ type: 'decrement' })}>Subtract</button>
+        <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
       </h3>
     </div>
   );
