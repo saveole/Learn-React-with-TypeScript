96bd87cedb25df720dcdaa00c3e09ee43ec11f92
saveole <xgz.cz@foxmail.com>
2023-08-11 14:11:16 +0800

    React Hooks - useState usage

--- ch4/sec1-effect-hook/app/src/PersonScore.tsx
+++ ch4/sec1-effect-hook/app/src/PersonScore.tsx
@@ -1,11 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getPerson } from './getPerson';
 
 export function PersonScore() {
+  const [name, setName] = useState<string | undefined>();
+  const [score, setScore] = useState(0);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    getPerson().then((person) => console.log(person));
+    getPerson().then((person) => {
+      setLoading(false);
+      setName(person.name);
+    });
   }, []);
-  return null;
+
+  if (loading) {
+    return <div>Loading ...</div>;
+  }
+  return (
+    <div>
+      <h3>
+        {name}, {score}
+        <br />
+        <button onClick={() => setScore(score + 1)}>Add</button>
+        <button onClick={() => setScore(score - 1)}>Subtract</button>
+        <button onClick={() => setScore(0)}>Reset</button>
+      </h3>
+    </div>
+  );
 }
 
 /** 
