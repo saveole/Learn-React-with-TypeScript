40a1804e01f9d94090404de3ca76be63a45e316f
saveole <xgz.cz@foxmail.com>
2023-08-11 18:09:10 +0800

    tailwindcss - Alert component not cented yet...

---
+++ ch5/tailwind-css/src/Alert.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+
+type Props = {
+  type?: string;
+  heading: string;
+  children: React.ReactNode;
+  closable?: boolean;
+  onClose?: () => void;
+};
+
+export function Alert({ type = 'information', heading, children, closable, onClose }: Props) {
+  const [visible, setVisible] = useState(true);
+  if (!visible) {
+    return <div>Gone!</div>;
+  }
+  function handleCloseClick() {
+    setVisible(false);
+    if (onClose) {
+      onClose();
+    }
+  }
+  return (
+    <div
+      className={`inline-flex flex-col text-left px-4 py-3 rounded-md border-1 border-transparent ${
+        type === 'warning' ? 'text-amber-900' : 'text-teal-900'
+      } ${type === 'warning' ? 'bg-amber-50' : 'bg-teal-50'}`}
+    >
+      <div className="flex items-center mb-1">
+        <span
+          className="w-7"
+          role="img"
+          aria-label={type === 'warning' ? 'Warning' : 'Information'}
+        >
+          {type === 'warning' ? '⚠' : 'ℹ️'}
+        </span>
+        <span className="font-bold">{heading}</span>
+      </div>
+      {closable && (
+        <button
+          className="border-none bg-transparent ml-auto cursor-pointer"
+          aria-label="Close"
+          onClick={handleCloseClick}
+        >
+          <span role="img" aria-label="Close">
+            ❌
+          </span>
+        </button>
+      )}
+      <div className="ml-7 text-black">{children}</div>
+    </div>
+  );
+}
---
+++ ch5/tailwind-css/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import { Alert } from './Alert';
+
+function App() {
+  return (
+    <div className="container">
+      <Alert heading="Success" closable>
+        Everything is really good!
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
