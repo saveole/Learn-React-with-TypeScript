e00da62af74d0e12130178d2df0d5c37f025a0ad
saveole <xgz.cz@foxmail.com>
2023-08-16 08:29:49 +0800

    react router for product list

---
+++ ch6/react-router/src/data/products.ts
@@ -0,0 +1,40 @@
+export type Product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+};
+
+export const products = [
+  {
+    description:
+      "A collection of navigational components that compose declaratively with your app",
+    id: 1,
+    name: "react router",
+    price: 8,
+  },
+  {
+    descrition: "A lib that helps manage state across your app",
+    id: 2,
+    name: "react redux",
+    price: 12,
+  },
+  {
+    descrition: "A lib that helps you interact with a REST API",
+    id: 3,
+    name: "react apollo",
+    price: 10,
+  },
+  {
+    descrition: "A lib that helps you implement robust forms",
+    id: 4,
+    name: "react hook form",
+    price: 9,
+  },
+  {
+    descrition: "A lib that provides utility CSS classes",
+    id: 5,
+    name: "tailwind css",
+    price: 7,
+  },
+];
