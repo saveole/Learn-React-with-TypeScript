a07cec9a34af3ec4b07931c640000ec043a61550
saveole <xgz.cz@foxmail.com>
2023-08-16 08:43:15 +0800

    use Link to nav header

--- ch6/react-router/src/Routes.tsx
+++ ch6/react-router/src/Routes.tsx
@@ -1,7 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ProductsPage } from "./pages/ProductPage";
+import { Header } from "./Header";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header />,
+  },
   {
     path: "products",
     element: <ProductsPage />,
