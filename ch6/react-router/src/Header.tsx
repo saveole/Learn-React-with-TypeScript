a89d9eb15cb2d72ff40128cbd7d7c9851ea2be50
saveole <xgz.cz@foxmail.com>
2023-08-18 09:12:51 +0800

    useSearchParams

--- ch6/react-router/src/Header.tsx
+++ ch6/react-router/src/Header.tsx
@@ -1,9 +1,26 @@
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, useSearchParams } from "react-router-dom";
+import { FormEvent } from "react";
 import logo from "./logo.svg";
 
 export function Header() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  function handleSearchSubmit(e: FormEvent<HTMLFormElement>) {
+    const formData = new FormData(e.currentTarget);
+    const search = formData.get("search") as string;
+    setSearchParams({ search });
+  }
   return (
     <header className="text-center text-slate-50 bg-slate-900 h-40 p-5">
+      <form className="relative text-right" onSubmit={handleSearchSubmit}>
+        <input
+          className="absolute right-0 top-0 rounded py-2 px-3 text-gray-700"
+          type="search"
+          name="search"
+          placeholder="Search"
+          defaultValue={searchParams.get("search") ?? ""}
+        />
+      </form>
+
       <Link to="">
         <img src={logo} alt="Logo" className="inline-block h-20" />
       </Link>
