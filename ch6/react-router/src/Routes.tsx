7964b7ec0b236d1ef10f3c1dfabc04f0bd86630c
saveole <xgz.cz@foxmail.com>
2023-08-22 15:06:41 +0800

    React Router - Lazy Loading

--- ch6/react-router/src/Routes.tsx
+++ ch6/react-router/src/Routes.tsx
@@ -3,7 +3,10 @@ import { ProductsPage } from "./pages/ProductsPage";
 import { ProductPage } from "./pages/ProductPage";
 import { ErrorPage } from "./pages/ErrorPage";
 import { HomePage } from "./pages/HomePage";
+import { lazy, Suspense } from "react";
 import App from "./App";
+
+const AdminPage = lazy(() => import("./pages/AdminPage"));
 
 const router = createBrowserRouter([
   {
@@ -22,6 +25,16 @@ const router = createBrowserRouter([
       {
         path: "products/:id",
         element: <ProductPage />,
+      },
+      {
+        path: "admin",
+        element: (
+          <Suspense
+            fallback={<div className="text-center p-5 text-xl">Loading...</div>}
+          >
+            <AdminPage />
+          </Suspense>
+        ),
       },
     ],
   },
---
+++ ch6/react-router/src/pages/AdminPage.tsx
@@ -0,0 +1,10 @@
+export default function AdminPage() {
+  return (
+    <div className="text-center p-5 text-xl">
+      <h1 className="text-xl text-slate-500">Admin Panel</h1>
+      <p className="text-base text-slate-100">
+        You shouldn't come here often becaus I'm lazy
+      </p>
+    </div>
+  );
+}
