7cc7ad9e6dba703d3778a3de42ffe298d16cc6a3
saveole <xgz.cz@foxmail.com>
2023-08-30 18:02:13 +0800

    react-hook-form handle form submit and validation

---
+++ ch7/react_hook_form/src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./App.css";
+import { ContactPage } from "./ContactPage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import { ThankYouPage } from "./ThankYouPage";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navigate to="contact" />,
+    },
+    {
+      path: "/contact",
+      element: <ContactPage />,
+    },
+    {
+      path: "/thank-you/:name",
+      element: <ThankYouPage />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+export default App;
