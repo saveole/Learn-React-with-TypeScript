0677882791344b81d50fadd18a26702625dabe0d
save_ole <xgz.cz@foxmail.com>
2023-09-06 21:19:40 +0800

    ReactNode render props

--- ch11/reusable-components/src/Checklist/Checklist.tsx
+++ ch11/reusable-components/src/Checklist/Checklist.tsx
@@ -1,16 +1,27 @@
-import { ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, ReactNode } from 'react';
 
 type Props<Data> = {
   data: Data[];
   id: keyof Data;
   primary: keyof Data;
   secondary: keyof Data;
+  renderItem: (item: Data) => ReactNode;
 } & ComponentPropsWithoutRef<'ul'>;
 
-export function Checklist<Data>({ data, id, primary, secondary, ...ulProps }: Props<Data>) {
+export function Checklist<Data>({
+  data,
+  id,
+  primary,
+  secondary,
+  renderItem,
+  ...ulProps
+}: Props<Data>) {
   return (
     <ul className="bg-gray-300 rounded p-10" {...ulProps}>
       {data.map((item) => {
+        if (renderItem) {
+          return renderItem(item);
+        }
         const idValue = item[id] as unknown;
         if (typeof idValue !== 'string' && typeof idValue !== 'number') {
           return null;
