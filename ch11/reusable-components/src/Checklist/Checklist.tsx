b775bb7d18c8be5ba94bba295e88c816ef79d3ec
save_ole <xgz.cz@foxmail.com>
2023-09-07 06:10:14 +0800

    useState and checked box

--- ch11/reusable-components/src/Checklist/Checklist.tsx
+++ ch11/reusable-components/src/Checklist/Checklist.tsx
@@ -1,27 +1,26 @@
-import { ComponentPropsWithoutRef, ReactNode } from 'react';
+import { ComponentPropsWithoutRef, useState } from 'react';
+import { IdValue } from './types';
 
 type Props<Data> = {
   data: Data[];
   id: keyof Data;
   primary: keyof Data;
   secondary: keyof Data;
-  renderItem: (item: Data) => ReactNode;
 } & ComponentPropsWithoutRef<'ul'>;
 
-export function Checklist<Data>({
-  data,
-  id,
-  primary,
-  secondary,
-  renderItem,
-  ...ulProps
-}: Props<Data>) {
+export function Checklist<Data>({ data, id, primary, secondary, ...ulProps }: Props<Data>) {
+  const [checkedIds, setCheckedIds] = useState<IdValue[]>([]);
+  // https://javascript.info/currying-partials
+  const handleCheckChange = (checkedId: IdValue) => () => {
+    const isChecked = checkedIds.includes(checkedId);
+    let newCheckedIds = isChecked
+      ? checkedIds.filter((itemCheckedid: IdValue) => itemCheckedid !== checkedId)
+      : checkedIds.concat(checkedId);
+    setCheckedIds(newCheckedIds);
+  };
   return (
     <ul className="bg-gray-300 rounded p-10" {...ulProps}>
       {data.map((item) => {
-        if (renderItem) {
-          return renderItem(item);
-        }
         const idValue = item[id] as unknown;
         if (typeof idValue !== 'string' && typeof idValue !== 'number') {
           return null;
@@ -34,8 +33,17 @@ export function Checklist<Data>({
 
         return (
           <li key={idValue} className="bg-white p-6 shadow rounded mb-4 last:mb-0">
-            <div className="text-xl text-gray-800 pb-1">{pText}</div>
-            {typeof sText === 'string' && <div className="text-sm text-gray-500">{sText}</div>}
+            <label className="flex items-center">
+              <input
+                type="checkbox"
+                checked={checkedIds.includes(idValue)}
+                onChange={handleCheckChange(idValue)}
+              />
+              <div className="ml-2">
+                <div className="text-xl text-gray-800 pb-1">{pText}</div>
+                {typeof sText === 'string' && <div className="text-sm text-gray-500">{sText}</div>}
+              </div>
+            </label>
           </li>
         );
       })}
